46c7de458d35a43981cf32cbf87c3199b84d8b74
DELL <DELL@DESKTOP-N5HRQG1>
2021-08-17 08:54:16 +0530

    First Commit!

---
+++ Sample_app/src/main/java/sample_app/MainClass.java
@@ -0,0 +1,10 @@
+package sample_app;
+
+public class MainClass {
+	public static void main(String[] agrs) {
+		System.out.println("Hello World!");
+		System.out.println("Hello Wold!");
+		System.out.println("Hello Wrld!");
+		System.out.println("Hello Wod!");
+	}
+}
